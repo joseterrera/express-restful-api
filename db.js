333824a272045a6a127da75dcd23794ba54040d0
Jose Terrera <joseterrera@yahoo.com>
2020-10-27 13:17:14 -0700

    modifying db.js, setting up project

--- db.js
+++ db.js
@@ -1,2 +1,24 @@
 /** Database setup for BizTime. */
 
+
+const { Client } = require("pg");
+
+
+let DB_URI;
+
+if (process.env.NODE_ENV === "test") {
+  DB_URI = "postgresql:///biztime_test";
+} else {
+  DB_URI = "postgresql:///biztime";
+}
+
+
+
+const client = new Client({
+  connectionString: DB_URI
+});
+
+client.connect();
+
+
+module.exports = client;
